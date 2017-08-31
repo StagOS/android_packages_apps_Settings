8faf1a6a4e739cd4bbabf24ebd585c1d9bc169f1
maxwen <max.weninger@gmail.com>
2017-08-31 23:16:42 +0800

    Add Horns dashboard
    Change-Id: I8543b8af6f508e2ead192c7ee1fdc9603cc5b06d

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -201,6 +201,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.stag.horns.Horns;
+
 public class SettingsGateway {
 
     /**
@@ -390,6 +392,7 @@ public class SettingsGateway {
             ScreenTimeoutSettings.class.getName(),
             ResetNetwork.class.getName(),
             VibrationIntensitySettingsFragment.class.getName(),
+            Horns.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -436,5 +439,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.HornsActivity.class.getName(),
     };
 }
