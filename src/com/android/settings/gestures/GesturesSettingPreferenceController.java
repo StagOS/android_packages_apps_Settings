36699bd0a7e6c0500458ff0405afe20d1f55d683
Chris Crump <chris@cypheros.co>
2018-10-29 18:09:29 +0100

    Settings: Long press power button to toggle torch [2/2]
    * Animation by Sahul Krishnan, remade to lottie by Andrew Fluck
    * Adapt to Android 11 by @mickaelmendes50
    
    Change-Id: Ibdce3644ba1e794120a268064843349b9fa432a9
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -82,6 +82,7 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
         controllers.add(new ScreenOffUdfpsPreferenceController(context, FAKE_PREF_KEY)
                 .setConfig(ambientDisplayConfiguration));
         controllers.add(new DoubleTapAmbientPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new PowerButtonTorchGesturePreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
 }
