16132601042d8023b81b54d596d8ce9a3f1832a2
Ido Ben-Hur <idoybh2@gmail.com>
2022-10-31 19:21:14 +0200

    Settings: Add an activity for saved Bluetooth devices fragment
    Needed for the bluetooth panel "see all" button to actually function
    
    Change-Id: I76c24982a2aa6d47970b932694c82aca53d0556c
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -440,5 +440,6 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.HornsActivity.class.getName(),
+            Settings.PreviouslyConnectedDeviceDashboardActivity.class.getName(),
     };
 }
