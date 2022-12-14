807df056cfe646774856cd91f98d1047af187e6f
vjspranav <pranavasri@live.in>
2022-12-14 15:39:31 +0000

    Revert "Settings: panels: Add Bluetooth panel"
    This reverts commit b319aa8eea36dc058ca70dadac6ad10a92e002a2.

--- src/com/android/settings/panel/PanelFeatureProviderImpl.java
+++ src/com/android/settings/panel/PanelFeatureProviderImpl.java
@@ -63,8 +63,6 @@ public class PanelFeatureProviderImpl implements PanelFeatureProvider {
                 }
             case Settings.Panel.ACTION_MOBILE_DATA:
                 return MobileDataPanel.create(context);
-            case Settings.Panel.ACTION_BLUETOOTH:
-                return BluetoothPanel.create(context);
         }
 
         throw new IllegalStateException("No matching panel for: " + panelType);
