vjspranav/stag_packages_apps_Settings

Add Horns dashboard
Change-Id: I8543b8af6f508e2ead192c7ee1fdc9603cc5b06d

Settings: Make Horns icon match others for Pie

Also fix SettingsActivity

Change-Id: Id874b022eece92dde25abd2572880161f37d9f14

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.stag.horns.Horns;
+
 public class SettingsGateway {
 
     /**
@@ -283,7 +285,8 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
-            GlobalActionsPanelSettings.class.getName()
+            GlobalActionsPanelSettings.class.getName(),
+            Horns.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -322,5 +325,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.HornsActivity.class.getName(),
     };
-}
+            }

StagOS/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -180,6 +180,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.stag.horns.Horns;
+
 public class SettingsGateway {
 
     /**
@@ -347,7 +349,8 @@ public class SettingsGateway {
             MobileNetworkSettings.class.getName(),
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
-            TurnScreenOnDetails.class.getName()
+            TurnScreenOnDetails.class.getName(),
+            Horns.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -390,5 +393,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.HornsActivity.class.getName(),
     };
 }