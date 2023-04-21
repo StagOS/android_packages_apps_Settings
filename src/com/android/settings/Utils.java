maldini03/android_packages_apps_Settings-2

Settings: Add toggle for Custom UI [2/2]
* TODO: Refresh systemUI

Change-Id: I541f18ff25280f7b117aeb949a14f2efc1ab5b5c
Signed-off-by: aswin7469 <aswinas@pixysos.com>

src/com/android/settings/Utils.java
@@ -77,6 +77,7 @@ import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Profile;
 import android.provider.ContactsContract.RawContacts;
+import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.Spannable;
@@ -118,6 +119,8 @@ import com.android.settingslib.widget.AdaptiveIcon;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
 
 public final class Utils extends com.android.settingslib.Utils {
 
@@ -1220,7 +1223,9 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColor(Context context) {
-        return getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+        int customUIToggleValue = Settings.System.getInt(context.getContentResolver(),
+                 CUSTOM_UI_TOGGLE, 0);
+        return getColorAttrDefaultColor(context, customUIToggleValue == 1 ? android.R.attr.colorAccent : android.R.attr.textColorSecondary);
     }
 
     /**

StagOS/android_packages_apps_Settings

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/settings/Utils.java
@@ -18,6 +18,7 @@ package com.android.settings;
 
 import static android.content.Intent.EXTRA_USER;
 import static android.content.Intent.EXTRA_USER_ID;
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
 import static android.text.format.DateUtils.FORMAT_ABBREV_MONTH;
 import static android.text.format.DateUtils.FORMAT_SHOW_DATE;
 
@@ -77,6 +78,7 @@ import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.Profile;
 import android.provider.ContactsContract.RawContacts;
+import android.provider.Settings;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.Spannable;
@@ -1222,7 +1224,9 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     @ColorInt
     public static int getHomepageIconColor(Context context) {
-        return getColorAttrDefaultColor(context, android.R.attr.colorAccent);
+        int customUIToggleValue = Settings.System.getInt(context.getContentResolver(),
+                 CUSTOM_UI_TOGGLE, 0);
+        return getColorAttrDefaultColor(context, customUIToggleValue == 1 ? android.R.attr.colorAccent : android.R.attr.textColorPrimary);
     }
 
     /**