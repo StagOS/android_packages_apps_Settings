bf3f297592fd3074f0390265ac299ce43662f9e3
vjspranav <pranavasri@live.in>
2024-04-27 10:50:35 +0200

    Revert "SelinuxStatus: Use runtime to display Selinux status correctly"
    This reverts commit 98da6a33cd2ee1fd1d46b95863bf104f0c45d599.

--- src/com/android/settings/deviceinfo/SELinuxStatusPreferenceController.java
+++ src/com/android/settings/deviceinfo/SELinuxStatusPreferenceController.java
@@ -19,15 +19,9 @@ import android.content.Context;
 import android.os.SELinux;
 import android.os.SystemProperties;
 import android.text.TextUtils;
-import android.util.Log;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-
-import java.io.BufferedReader;
-import java.io.StringBufferInputStream;
-import java.io.InputStreamReader;
-import java.lang.Runtime;
 
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
@@ -37,7 +31,6 @@ public class SELinuxStatusPreferenceController extends AbstractPreferenceControl
         PreferenceControllerMixin {
 
     private static final String KEY_SELINUX_STATUS = "selinux_status";
-    private static final String TAG = "SelinuxStatusCtrl";
 
     public SELinuxStatusPreferenceController(Context context) {
         super(context);
@@ -52,35 +45,6 @@ public class SELinuxStatusPreferenceController extends AbstractPreferenceControl
     public String getPreferenceKey() {
         return KEY_SELINUX_STATUS;
     }
-    	
-    public boolean isSeLinuxEnforcing() {
-		StringBuffer output = new StringBuffer();
-		Process p;
-		try {
-			p = Runtime.getRuntime().exec("getenforce");
-			p.waitFor();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line = "";
-			while ((line = reader.readLine())!= null) {
-				output.append(line);
-			}
-		} catch (Exception e) {
-			Log.e(TAG, "OS does not support getenforce");
-			// If getenforce is not available to the device, assume the device is not enforcing
-			e.printStackTrace();
-			return false;
-		}
-		String response = output.toString();
-		if ("Enforcing".equals(response)) {
-			return true;
-		} else if ("Permissive".equals(response)) {
-			return false;
-		} else {
-			Log.e(TAG, "getenforce returned unexpected value, unable to determine selinux!");
-			// If getenforce is modified on this device, assume the device is not enforcing
-			return false;
-		}
-	}
 
     @Override
     public void displayPreference(PreferenceScreen screen) {
@@ -92,10 +56,7 @@ public class SELinuxStatusPreferenceController extends AbstractPreferenceControl
         if (!SELinux.isSELinuxEnabled()) {
             String status = mContext.getResources().getString(R.string.selinux_status_disabled);
             pref.setSummary(status);
-        } else if (isSeLinuxEnforcing()) {
-            String status = mContext.getResources().getString(R.string.selinux_status_enforcing);
-            pref.setSummary(status);
-        } else {
+        } else if (!SELinux.isSELinuxEnforced()) {
             String status = mContext.getResources().getString(R.string.selinux_status_permissive);
             pref.setSummary(status);
         }
